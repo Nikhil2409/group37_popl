8dc2db2f52423399b6a07d236bcf275ba2ec514c
PreetShah67 <101554030+PreetShah67@users.noreply.github.com>
2023-11-20 23:18:57 +0530

    Create test_rust.rs

---
+++ code-external/test_rust.rs
@@ -0,0 +1,14 @@
+use std::time::Instant;
+
+// Neural Network and training code (unchanged)
+
+fn main() {
+    let start_training = Instant::now();
+
+    // Training code
+
+    let training_duration = start_training.elapsed();
+    println!("Training Time: {:?}", training_duration);
+
+    // Testing code (unchanged)
+}
