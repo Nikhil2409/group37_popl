0cd763060fc0033f0aa54bbd997f4b7aba9d3353
PreetShah67 <101554030+PreetShah67@users.noreply.github.com>
2023-11-22 23:48:36 +0530

    Create tests_cpp.cpp

---
+++ tests/tests_cpp.cpp
@@ -0,0 +1,20 @@
+vector<vector<double>> training_inputs = {
+    {0.0, 0.0},
+    {0.0, 1.0},
+    {1.0, 0.0},
+    {1.0, 1.0}
+};
+
+vector<vector<double>> training_targets = {
+    {0.0},
+    {1.0},
+    {1.0},
+    {0.0}
+};
+
+vector<vector<double>> test_inputs = {
+    {0.0, 0.0},
+    {0.0, 1.0},
+    {1.0, 0.0},
+    {1.0, 1.0}
+};
