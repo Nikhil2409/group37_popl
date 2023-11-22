fbc71e42dbefefb7befbabe1d7752363fa458a4b
PreetShah67 <101554030+PreetShah67@users.noreply.github.com>
2023-11-22 23:50:11 +0530

    Create tests_rust.rs

---
+++ tests/tests_rust.rs
@@ -0,0 +1,12 @@
+let training_inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+    let training_targets = vec![
+        vec![0.0],
+        vec![1.0],
+        vec![1.0],
+        vec![0.0],
+    ];
